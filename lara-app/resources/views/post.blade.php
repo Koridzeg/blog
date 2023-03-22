0d7e64b712dd9b7e63f5bd8afe9b738c699cca86
Koridzeg <giorgi.koridze@redberry.ge>
2023-03-22 19:38:34 +0400

    feat: setup routes&views

---
+++ lara-app/resources/views/post.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<article>
+    <h1><a href="/post"> My first post </a></h1>
+
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde, dolore cumque. Accusamus illum repellat nulla. Natus, voluptate. Nobis adipisci accusantium architecto tempora quod odit doloribus.</p>
+</article>
+
+<a href="/">Go Back</a>
+</body>
+</html>
