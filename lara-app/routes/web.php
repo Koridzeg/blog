634e44aefe5a448002c699146c31ef5d2cc8070e
Koridzeg <giorgi.koridze@redberry.ge>
2023-03-22 22:00:20 +0400

    feat: added caching for faster response times

--- lara-app/routes/web.php
+++ lara-app/routes/web.php
@@ -20,11 +20,14 @@ Route::get('/', function () {
 Route::get('posts/{post}', function ($slug) {
     $path = __DIR__ . "/../resources/posts/{$slug}.html";
 
-    if(!  file_exists($path)){
+    if(!file_exists($path)){
         return redirect('/');
     }
 
-    $post = file_get_contents($path);
+   $post = cache()->remember("posts.{$slug}", 5, function () use ($path) {
+    return file_get_contents($path);
+    });
+    
 
     return view('post', [
         'post' => $post
