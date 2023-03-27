83bb29cf6c0f515c2a8ba4bf3bdb482e98213d20
Koridzeg <giorgi.koridze@redberry.ge>
2023-03-27 14:42:31 +0400

    feat: search functionallity

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
@@ -17,24 +18,9 @@ use Illuminate\Support\Facades\File;
 |
 */
 
-Route::get('/', function () {
-    
+Route::get('/', [PostController::class, 'index'])->name('home');
 
-    $posts = Post::latest()->get();
-
-
-    return view('posts', [
-        'posts' => $posts,
-        'categories' => Category::all()
-    ]);
-})->name('home');
-
-Route::get('posts/{post:slug}', function (Post $post) {
-
-    return view('post', [
-        'post' => $post
-    ]);
-});
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 
 Route::get('categories/{category:slug}', function (Category $category) {
