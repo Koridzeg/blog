b78d0cd019d36c7c7e96cde1ad0dc8353fd2ee76
Koridzeg <giorgi.koridze@redberry.ge>
2023-03-27 13:44:50 +0400

    feat: add/seperate reusable components

--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,10 @@ Route::get('/', function () {
 
 
     return view('posts', [
-        'posts' => $posts
+        'posts' => $posts,
+        'categories' => Category::all()
     ]);
-});
+})->name('home');
 
 Route::get('posts/{post:slug}', function (Post $post) {
 
@@ -38,13 +39,16 @@ Route::get('posts/{post:slug}', function (Post $post) {
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all()
     ]);
-});
+})->name('category');
 
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
-        'posts' => $author->posts
+        'posts' => $author->posts,
+        'categories' => Category::all()
     ]);
 });
